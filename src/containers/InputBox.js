4e1abe44d6fc0ee6093cd4bba628b8b3d1dc148d
megholova <Megholov.Mukherjee@timeinc.com>
2018-07-19 21:26:46 +0530

    clearing suggestion box on clicking outside of container

--- src/containers/InputBox.js
+++ src/containers/InputBox.js
@@ -7,23 +7,44 @@ import SearchIcon from '../images/SearchIcon';
 export default class Autosuggest extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            items: this.props.items,
+        }
+    }
+
+    componentWillReceiveProps(nextProps) {
+        this.setState({
+            items: nextProps.items,
+        })
+    }
+
+    clearSuggestions(e) {
+        if (!(e.target.className === 'link' || e.target.className === 'input-box')) {
+            this.setState({
+                items: []
+            });
+        }
     }
 
     render() {
-        let { keyword,items} = this.props;
+        let { keyword } = this.props;
+        let { items } = this.state;
         return (
-            <form>
-                <div>AUTO SUGGEST BOX </div>
-                <div className="search-container">
-                    <input className="input-box" value={keyword} onChange={(e) => this.props.handleInputChange(e)} />
-                    <button className="search-button" type="submit"><SearchIcon /></button>
+            <div className="auto-suggest" onClick={(e) => this.clearSuggestions(e)}>
+                <div className="search">
+                    <div>AUTO SUGGEST BOX </div>
+                    <div className="search-container">
+                        <input className="input-box" value={keyword} onChange={(e) => this.props.handleInputChange(e)} />
+                        {items.length > 0 && <div className="suggestion-box">
+                            {items.map((item, index) => {
+                                return (<a className="links" href={item.url} key={index}>{item.name}</a>)
+                            })}
+                        </div>}
+                        <button className="search-button" type="submit"><SearchIcon /></button>
+                    </div>
+
                 </div>
-                {items.length > 0 && <div className="suggestion-box">
-                    {items.map((item) => {
-                        return (<a className="links" href={item.url} >{item.name}</a>)
-                    })}
-                </div>}
-            </form>
+            </div>
         )
     }
 }
